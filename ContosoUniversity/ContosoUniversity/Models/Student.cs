e4e01280c788f54f3fb74783085020012dc39e5c
lddz <2603792595@qq.com>
2019-01-03 18:13:20 +0800

    学生选课项目

---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Display(Name ="姓名")]
+        public string Name { get; set; }
+
+        [Display(Name = "注册日期")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
